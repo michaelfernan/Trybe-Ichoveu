c08a01d1ccc1cfadb875a5423026c7a7b526802c
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-08-10 09:07:45 -0300

    requisito 1

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,22 @@
 // Remova os comentários a medida que for implementando as funções
+//
+const TOKEN = 'cedc300c832d4550b09124423230908';
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  try {
+    const response = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
+    const data = await response.json();
+
+    if (data.length === 0) {
+      window.alert('Nenhuma cidade encontrada');
+      return [];
+    }
+
+    return data;
+  } catch (error) {
+    console.error('Erro ao buscar cidades:', error);
+    return [];
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
